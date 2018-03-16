497b40275f0baf5d5a6b434960b132a0b2dba32a
Brendan <brendangraff@gmail.com>
2018-03-16 16:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  recipes.key = value;
+  recipes[key] = value;
   return recipes;
 }
 
